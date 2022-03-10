db6651a836efe7f601727b263e3aeb1b25f89333
Liu yuyang <louisfred141@outlook.com>
2022-03-10 15:34:43 +0900

    Update wallpaper-cam.py

--- wallpaper-cam.py
+++ wallpaper-cam.py
@@ -2,79 +2,43 @@ import ctypes
 from ctypes.wintypes import RGB
 from datetime import datetime
 from importlib.resources import path
-from multiprocessing.connection import wait
-from tkinter import CENTER
-from turtle import width
 import cv2 as cv
 import time
 from threading import Thread,Lock
 from cv2 import VideoCapture
 from cv2 import CAP_FFMPEG
 import numpy as np
-
-import sys
 import os
 import logging
-
 from outcome import capture
 logging.basicConfig(level=logging.DEBUG)
 from PIL import Image, ImageDraw, ImageFont
-import traceback
 from datetime import datetime
-import subprocess
-#import psutil
-import socket
-import random
 import json
-import textwrap
 import pyowm
-from bs4 import BeautifulSoup
 import requests
-import re
-from selenium import webdriver
-from html.parser import HTMLParser
-from os import linesep
-import folium
+
 
 picdir = os.path.join('C:/Users/Louis/Pictures/screen/cam/')
+#Place for image storage.
 
 DATEFORMAT = "%a %x"
 TIMEFORMAT = "%H:%M"
-
-#BOUNCETIME = 500
-
-
-weather_icon_dict = {200 : "6", 201 : "6", 202 : "6", 210 : "6", 211 : "6", 212 : "6", 
-                     221 : "6", 230 : "6" , 231 : "6", 232 : "6", 
-
-                     300 : "7", 301 : "7", 302 : "8", 310 : "7", 311 : "8", 312 : "8",
-                     313 : "8", 314 : "8", 321 : "8", 
- 
-                     500 : "7", 501 : "7", 502 : "8", 503 : "8", 504 : "8", 511 : "8", 
-                     520 : "7", 521 : "7", 522 : "8", 531 : "8",
-
-                     600 : "V", 601 : "V", 602 : "W", 611 : "X", 612 : "X", 613 : "X",
-                     615 : "V", 616 : "V", 620 : "V", 621 : "W", 622 : "W", 
-
-                     701 : "M", 711 : "M", 721 : "M", 731 : "M", 741 : "M", 751 : "M",
-                     761 : "M", 762 : "M", 771 : "M", 781 : "M", 
-
-                     800 : "1", 
-
-                     801 : "H", 802 : "N", 803 : "N", 804 : "Y"
-}
-
-
-
+#Formatting
 
 ut = time.time()
-print(ut)
+#print(ut)
 
+#open camera and avoid overexposure
 capture = cv.VideoCapture() 
 capture.open(0)   
+
+#get image
 ret,img = capture.read()
 
+
 class WebcamVideoStream :
+    #grab the frame from a stream to avoid overexposure
     def __init__(self, src = 0,width = 1920,height = 1080) :
         self.stream = cv.VideoCapture(src)
         self.stream.set(cv.CAP_PROP_FRAME_WIDTH, width)
@@ -115,14 +79,16 @@ class WebcamVideoStream :
 
 def setimg(img):
     cv.imwrite('C:\\Users\\Louis\\Pictures\\screen\\cam\\temp.jpg',img)
-    print("Image setted")
+    print("Image Set")
 
 
 def set_wallpaper(path):
+#set image use windows system api
     SPI_SETDESKWALLPAPER = 20
     path = 'C:\\Users\\Louis\\Pictures\\screen\\cam\\temp.jpg'
     ctypes.windll.user32.SystemParametersInfoW(20,0,path,1)
     return;
+
 
 def set_alldata():
     with Image.open("C:/Users/Louis/Pictures/screen/cam/temp.jpg").convert("RGBA") as base:
@@ -130,7 +96,7 @@ def set_alldata():
         # make a blank image for the text, initialized to transparent text color
         txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
 
-        # get a font
+        # get a font you want
         fntb = ImageFont.truetype("C:/Users/Louis/Pictures/screen/cam/Futura Bold Italic font.ttf", 13)
         fnt = ImageFont.truetype("C:/Users/Louis/Pictures/screen/cam/Futura Medium Italic font.ttf", 14)
         fnw = ImageFont.truetype("C:/Users/Louis/Pictures/screen/cam/Futura Bold Italic font.ttf", 28)
@@ -151,8 +117,10 @@ def set_alldata():
         
         d.text((795, 230),timestring, font=fnw2,fill=(255, 255, 255, 255),align='right')
 
-        #time setted
+        #time set
         logging.info("Clock updated.")
+        
+        #set city ID
         city_id = 2110683 #Tsukuba City
         owm = pyowm.OWM('')
         mgr = owm.weather_manager()
@@ -176,7 +144,9 @@ def set_alldata():
         windstr = 'WIND'+'@'+str(wind['speed'])+'m/s                               '+'Direction'+'@'+str(wind['deg'])+'°'
         desstr = str(weather.detailed_status)
         desstr = desstr.title()
-
+        
+        #draw UI
+        
         d.text((800, 380), "Tsukuba", font=fnw)
         d.text((800, 406), desstr, font = fnt)
         d.text((800, 435), windstr,font = fntb)
@@ -187,7 +157,9 @@ def set_alldata():
     
         
         logging.info("weather Updated")
-
+        
+        #set earthquake info, limit = 4 newest
+        
         earthquake = requests.get('https://api.p2pquake.net/v1/human-readable?limit=4')
         earthquake_data = earthquake.content.decode('utf-8')
         earthquake_publish_time = []
@@ -218,9 +190,9 @@ def set_alldata():
                 t4 = t3.get('latitude')+'  '+t3.get('longitude')
                 string1 = '-'+str(indexe+1)+'- '+'Time:'+time[5:19]+' @ '+center
                 string2 = 'Mag:'+mag+'  @ '+t4
-
-                #drawblk.text((5,5+indexe*10), str(t1), font = self.fonts.smallfont, fill = 0)
-
+                
+            # draw UI
+            
                 d.text((831, 760+indexe*40), string1, font = fnj,fill=(rgbpx[0],rgbpx[1],rgbpx[2],120))
                 d.text((830, 760+indexe*40), string1, font = fnj,fill=(255,255,255,120))
                 d.text((831, 780+indexe*40), string2, font = fnj,fill=(rgbpx[0],rgbpx[1],rgbpx[2],120))
@@ -238,39 +210,48 @@ def set_alldata():
             earthquakelog="Earthquake"+str( indexe) +" Updated"
             logging.info(earthquakelog)
 
-        print("PIL img setted.")
+        print("PIL img set.")
         out = Image.alpha_composite(base, txt)
         out = out.convert('RGB')
+        
+        #convert PIL object to OpenCV
+        
         opencv_out = np.array(out)
         opencv_out = opencv_out[:, :, ::-1].copy() 
-        #convert PIL object to OpenCV
+        
+        #over write image
+        
         cv.imwrite('C:\\Users\\Louis\\Pictures\\screen\\cam\\temp.jpg',opencv_out)
         print("OpenCV img setted.")
 
-
+#main process
 
 if __name__ == "__main__":
-
+    
+    #start cam
+    
     vs = WebcamVideoStream().start()
-
+    
+    #update period
+    
     minute = 15
     s=time.time()
     print(s)
+    
     while(True):
         s2 = time.time() 
-        ##print(s2-s)  
-        ##capture = cv.VideoCapture(0,cv.CAP_DSHOW)    
         img = vs.read()
-        #print('Img shows')
  
+        #wait 2 second and grab frame from cam stream
+  
         if(s2-s>2):
             setimg(img)
             set_alldata()
             set_wallpaper(path)
-            print('Set wallpaper!')
-            print('camera released')    
+            print('Set Wallpaper!')
+            print('Camera Released')    
             time.sleep(minute*60)
-            print('wallpaper updated!')
+            print('Wallpaper Updated!')
             s=s2
 
 
